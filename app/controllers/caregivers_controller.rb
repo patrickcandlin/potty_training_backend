d3fd31528332a31bb26ec9d450dfeaa1469659df
“Patrick <pctennislessons@gmail.com>
2020-03-14 17:24:22 -0600

    api working

--- app/controllers/caregivers_controller.rb
+++ app/controllers/caregivers_controller.rb
@@ -1,5 +1,5 @@
 class CaregiversController < ApplicationController
-
+    before_action :set_caregiver, only: :show
     def create
         caregiver = Caregiver.create(caregiver_params)
         if caregiver.valid?
@@ -9,7 +9,14 @@ class CaregiversController < ApplicationController
         end
     end
 
+    def show
+        render json: { user: CaregiverSerializer.new(@caregiver) }, status: :ok
+    end
+
     private
+    def set_caregiver
+        @caregiver = Caregiver.find(params[:id])
+    end
 
     def caregiver_params
         params.require(:caregiver).permit(:first_name, :last_name, :email, :password)
--- app/models/child.rb
+++ app/models/child.rb
@@ -2,4 +2,18 @@ class Child < ApplicationRecord
     has_many :potties
     validates :name, presence: true 
 
+    def list_potties
+        current_time = DateTime.now
+        current_date = current_time.to_date
+        
+        [ 
+            { "#{ current_date }" => self.potties.where(time: (current_date.to_datetime.midnight..current_date.to_datetime.end_of_day))}, 
+            { "#{ current_date - 1 }" => self.potties.where(time: (current_date.to_datetime.midnight- 1..current_date.to_datetime.end_of_day- 1))}, 
+            { "#{ current_date - 2 }" => self.potties.where(time: (current_date.to_datetime.midnight- 2..current_date.to_datetime.end_of_day- 2))}, 
+            { "#{ current_date - 3 }" => self.potties.where(time: (current_date.to_datetime.midnight- 3..current_date.to_datetime.end_of_day- 3))}, 
+            { "#{ current_date - 4 }" => self.potties.where(time: (current_date.to_datetime.midnight- 4..current_date.to_datetime.end_of_day- 4))}, 
+            { "#{ current_date - 5 }" => self.potties.where(time: (current_date.to_datetime.midnight- 5..current_date.to_datetime.end_of_day- 5))}, 
+            { "#{ current_date - 6 }" => self.potties.where(time: (current_date.to_datetime.midnight- 6..current_date.to_datetime.end_of_day- 6))}, 
+        ]
+    end
 end
